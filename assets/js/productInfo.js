c37a5f2e53d1980df7dcb21e486c33f860e051f9
Christopher Makousky <christopher.makousky@gmail.com>
2024-10-01 18:31:00 -0500

    Added javascript to display product info.

---
+++ assets/js/productInfo.js
@@ -0,0 +1,5 @@
+const learnMoreBook1 = document.querySelector('#learnMoreBook1');
+
+learnMoreBook1.addEventListener('click', function() {
+    document.getElementById("infoBook1").hidden = false;
+  });
