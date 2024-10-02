11f41f9ca16164ab4bd53109c104bc002dcc2b69
Christopher Makousky <christopher.makousky@gmail.com>
2024-10-02 02:29:03 -0500

    Add to cart function has been generalized for easier modification

--- assets/js/cartAdd.js
+++ assets/js/cartAdd.js
@@ -1,49 +1,51 @@
+//Query Selectors to locate the "Add to Cart" buttons
 const cartAddBook1 = document.querySelector('#cartAddBook1');
 const cartAddBook2 = document.querySelector('#cartAddBook2');
 const cartAddBook3 = document.querySelector('#cartAddBook3');
 const cartAddBook4 = document.querySelector('#cartAddBook4');
 
-localStorage.clear();
-const cartContents = [];
-const storedCart = [];
-let n = 0;
+//Set cartContents based on the presence of items in local storage
+if (localStorage.length === 0) {cartContents=[]}
+else{cartContents = JSON.parse(localStorage.getItem('cartContents'))};
 
-cartAddBook1.addEventListener('click', function(storedCart) {
-    cartContents[n]="Book 1";
-    console.log(cartContents);
+//Set cart array index counter
+if (localStorage.length === 0){n=0}
+else{n=cartContents.length};
+
+console.log(localStorage);
+console.log(cartContents);
+
+//Catalogue of books
+const bookCatalogue = ['Book 1','Book 2', 'Book 3', 'Book 4'];
+
+//Index selector for the catalogue of books
+let m = 0;
+
+//Function to add a book to the cart
+const addBook = function(){
+    cartContents[n]=bookCatalogue[m];
     localStorage.setItem('cartContents', JSON.stringify(cartContents));
-    console.log(localStorage);
-    storedCart = JSON.parse(localStorage.getItem('cartContents'));
-    console.log(storedCart);
     n++;
+    console.log(cartContents);
+};
+
+//Event Listeners
+cartAddBook1.addEventListener('click', function(){
+    m=0;
+    addBook();
 });
 
-cartAddBook2.addEventListener('click', function() {
-    cartContents[n]="Book 2";
-    console.log(cartContents);
-    localStorage.setItem('cartContents', JSON.stringify(cartContents));
-    console.log(localStorage);
-    storedCart = JSON.parse(localStorage.getItem('cartContents'));
-    console.log(storedCart);
-    n++;
+cartAddBook2.addEventListener('click', function(){
+    m=1;
+    addBook();
 });
 
-cartAddBook3.addEventListener('click', function() {
-    cartContents[n]="Book 3";
-    console.log(cartContents);
-    localStorage.setItem('cartContents', JSON.stringify(cartContents));
-    console.log(localStorage);
-    storedCart = JSON.parse(localStorage.getItem('cartContents'));
-    console.log(storedCart);
-    n++;
+cartAddBook3.addEventListener('click', function(){
+    m=2;
+    addBook();
 });
 
-cartAddBook4.addEventListener('click', function() {
-    cartContents[n]="Book 4";
-    console.log(cartContents);
-    localStorage.setItem('cartContents', JSON.stringify(cartContents));
-    console.log(localStorage);
-    storedCart = JSON.parse(localStorage.getItem('cartContents'));
-    console.log(storedCart);
-    n++;
+cartAddBook4.addEventListener('click', function(){
+    m=3;
+    addBook();
 });
--- assets/js/productInfo.js
+++ assets/js/productInfo.js
@@ -1,13 +1,16 @@
+//Query Selectors for the "Learn More" buttons
 const learnMoreBook1 = document.querySelector('#learnMoreBook1');
 const learnMoreBook2 = document.querySelector('#learnMoreBook2');
 const learnMoreBook3 = document.querySelector('#learnMoreBook3');
 const learnMoreBook4 = document.querySelector('#learnMoreBook4');
 
+//Query Selectors for the product information
 const infoBook1 = document.querySelector('#infoBook1');
 const infoBook2 = document.querySelector('#infoBook2');
 const infoBook3 = document.querySelector('#infoBook3');
 const infoBook4 = document.querySelector('#infoBook4');
 
+//Event Listeners
 learnMoreBook1.addEventListener('click', function() {
     infoBook1.hidden = false;
     infoBook2.hidden = true;
