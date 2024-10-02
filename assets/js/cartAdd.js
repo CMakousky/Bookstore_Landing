d875aab6b31027943df4dbd5500602eca5087371
Christopher Makousky <christopher.makousky@gmail.com>
2024-10-01 20:31:08 -0500

    Add to Cart button now adds the selected book to local storage.

--- assets/js/cartAdd.js
+++ assets/js/cartAdd.js
@@ -2,3 +2,39 @@ const cartAddBook1 = document.querySelector('#cartAddBook1');
 const cartAddBook2 = document.querySelector('#cartAddBook2');
 const cartAddBook3 = document.querySelector('#cartAddBook3');
 const cartAddBook4 = document.querySelector('#cartAddBook4');
+
+//localStorage.clear();
+const cartContents = [];
+let n = 0;
+
+cartAddBook1.addEventListener('click', function() {
+    cartContents[n]="Book 1";
+    console.log(cartContents);
+    localStorage.setItem('cartContents', JSON.stringify(cartContents));
+    console.log(localStorage);
+    n++;
+});
+
+cartAddBook2.addEventListener('click', function() {
+    cartContents[n]="Book 2";
+    console.log(cartContents);
+    localStorage.setItem('cartContents', JSON.stringify(cartContents));
+    console.log(localStorage);
+    n++;
+});
+
+cartAddBook3.addEventListener('click', function() {
+    cartContents[n]="Book 3";
+    console.log(cartContents);
+    localStorage.setItem('cartContents', JSON.stringify(cartContents));
+    console.log(localStorage);
+    n++;
+});
+
+cartAddBook4.addEventListener('click', function() {
+    cartContents[n]="Book 4";
+    console.log(cartContents);
+    localStorage.setItem('cartContents', JSON.stringify(cartContents));
+    console.log(localStorage);
+    n++;
+});
