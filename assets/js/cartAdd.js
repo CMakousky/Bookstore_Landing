9c965af542759583d4326774e4662c4ea08cf051
Christopher Makousky <christopher.makousky@gmail.com>
2024-10-01 19:26:43 -0500

    Started work on cart feature.

---
+++ assets/js/cartAdd.js
@@ -0,0 +1,4 @@
+const cartAddBook1 = document.querySelector('#cartAddBook1');
+const cartAddBook2 = document.querySelector('#cartAddBook2');
+const cartAddBook3 = document.querySelector('#cartAddBook3');
+const cartAddBook4 = document.querySelector('#cartAddBook4');
