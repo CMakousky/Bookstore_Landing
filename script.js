4ee44b1d202e2777b8f6caa115e57589f812bf21
Thida612 <angelic722@gmail.com>
2024-10-05 22:37:35 -0500

    added javascript file to this html

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-function message(){
-    var Name = document.getElementById('name');
-    var email = document.getElementById('email');
-    var msg = document.getElementById('msg');
-    const success = document.getElementById('success');
-    const danger = document.getElementById('danger');
+<!-- Include JavaScript before the closing body tag -->
+  
+    function sendMessage(){
+      var Name = document.getElementById('name');
+      var email = document.getElementById('email');
+      var msg = document.getElementById('msg');
+      const success = document.getElementById('success');
+      const danger = document.getElementById('danger');
 
-    if(Name.value === '' || email.value === '' || msg.value === ''){
-        danger.style.display = 'block';
+      if(Name.value === '' || email.value === '' || msg.value === ''){
+          danger.style.display = 'block';
+      } else {
+          setTimeout(() => {
+              Name.value = '';
+              email.value = '';
+              msg.value = '';
+          }, 2000);
+
+          success.style.display = 'block';
+      }
+
+      setTimeout(() => {
+          danger.style.display = 'none';
+          success.style.display = 'none';
+      }, 4000);
     }
-    else{
-        setTimeout(() => {
-            Name.value = '';
-            email.value = '';
-            msg.value = '';
-        }, 2000);
 
-        success.style.display = 'block';
+    function validateNewsletter(){
+      var newsletterEmail = document.getElementById('newsletter-email');
+      const newsletterSuccess = document.getElementById('newsletter-success');
+      const newsletterDanger = document.getElementById('newsletter-danger');
+
+      if(newsletterEmail.value === ''){
+          newsletterDanger.style.display = 'block';
+      } else {
+          setTimeout(() => {
+              newsletterEmail.value = '';
+          }, 2000);
+
+          newsletterSuccess.style.display = 'block';
+      }
+
+      setTimeout(() => {
+          newsletterDanger.style.display = 'none';
+          newsletterSuccess.style.display = 'none';
+      }, 4000);
     }
-
-
-    setTimeout(() => {
-        danger.style.display = 'none';
-        success.style.display = 'none';
-    }, 4000);
-
-}
+  
